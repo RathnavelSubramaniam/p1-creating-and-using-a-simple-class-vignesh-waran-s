558df1da906aa81a62a9846e4501455612ff7f0b
vignesh-waran-s <vignesh9944577169@gmail.com>
2024-12-11 06:39:27 +0000

    11

--- Practical1.java
+++ Practical1.java
@@ -1,42 +1,32 @@
-class Person {
+class Person
+{
     String firstName;
     String lastName;
-    int age;
-
-    // Default constructor
-    public Person() {
+    int age;;
+    public Person()
+    {
         firstName = "";
         lastName = "";
         age = 0;
     }
-
-    // Constructor with parameters
-    public Person(String firstName, String lastName, int age) {
+    public Person(String firstName,String lastName,int age){
         this.firstName = firstName;
         this.lastName = lastName;
         this.age = age;
     }
-
-    // Method to get full name
-    public String getFullName() {
-        return firstName + " " + lastName; // Add space between first and last name
+    public String getFullName(){
+    return firstName + " " + lastName;
     }
-}
-
-public class Practical1 
-{
-     public static void main(String[] args)
+    }
+    public class Practical1
     {
-        // Create two Person objects
-        Person person1 = new Person("John", "Doe", 30);
-        Person person2 = new Person("Alice", "Smith", 25);
-
-        // Correct the typo from personl to person1
-        System.out.println("Person 1: " + person1.getFullName());
-        System.out.println("Person 2: " + person2.getFullName());
-
-        // Correct the typo and calculate average age
-        double averageAge = (person1.age + person2.age) / 2.0;
-        System.out.println("Average Age: " + averageAge);
+    public static void main(String[] args)
+    {
+    Person person1 = new Person("John","Doe",30);
+    Person person2 = new Person("Alice","Smith",25);
+    System.out.println("Person1: " + person1.getFullName());
+    System.out.println("Person2: " + person2.getFullName());
+    double averageAge = (person1.age + person2.age)/2.0;
+    System.out.println("Average age: " + averageAge);
+        }
     }
-}
